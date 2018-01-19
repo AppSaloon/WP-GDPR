e22290757cf86f54b1ead993a35b8c43dad445f4
skurzynowski <sebastian.kurzynowski@gmail.com>
2018-01-19 10:03:21 +0100

    add array reduce and map

--- controller/controller-menu-page.php
+++ controller/controller-menu-page.php
@@ -3,6 +3,7 @@
 
 namespace wp_gdpr\controller;
 
+use wp_gdpr\lib\Appsaloon_Customtables;
 use wp_gdpr\lib\Appsaloon_Table_Builder;
 use wp_gdpr\lib\Gdpr_Container;
 
@@ -33,10 +34,15 @@ class Controller_Menu_Page {
 		$requesting_users = array_map( array( $this, 'map_request_status' ), $requesting_users );
 		//add checkbox input in every element with e-mail address
 		$requesting_users = array_map( array( $this, 'map_checkboxes_send_email' ), $requesting_users );
-
 		//show table object
 		$table = new Appsaloon_Table_Builder(
-			array( __('id', 'wp_gdpr'), __('e-mail', 'wp_gdpr'), __('status', 'wp_gdpr'), __('requested at', 'wp_gdpr'), __('resend e-mail', 'wp_gdpr') ),
+			array(
+				__( 'id', 'wp_gdpr' ),
+				__( 'e-mail', 'wp_gdpr' ),
+				__( 'requested at', 'wp_gdpr' ),
+				__( 'status', 'wp_gdpr' ),
+				__( 'resend e-mail', 'wp_gdpr' )
+			),
 			$requesting_users
 			, array( $form_content ) );
 
@@ -51,22 +57,13 @@ class Controller_Menu_Page {
 	public function get_requests_from_gdpr_table() {
 		global $wpdb;
 
-		$query = "SELECT * FROM {$wpdb->prefix}gdpr_requests";
+		$table_name = $wpdb->prefix . Appsaloon_Customtables::REQUESTS_TABLE_NAME;
+
+		$query = "SELECT * FROM $table_name";
 
 		return $wpdb->get_results( $query, ARRAY_A );
 	}
 
-	/**
-	 *
-	 * @return string
-	 */
-	public function get_delete_form_content() {
-		ob_start();
-		$controller = $this;
-		include_once GDPR_DIR . 'view/admin/delete-comments-form.php';
-
-		return ob_get_clean();
-	}
 	/**
 	 * @param $requesting_users
 	 *
@@ -112,13 +109,13 @@ class Controller_Menu_Page {
 
 		switch ( $data['status'] ) {
 			case 0:
-				$data['status'] = __('waiting for e-mail', 'wp_gdpr');
+				$data['status'] = __( 'waiting for e-mail', 'wp_gdpr' );
 				break;
 			case 1:
-				$data['status'] = __('e-mail send', 'wp_gdpr');
+				$data['status'] = __( 'e-mail send', 'wp_gdpr' );
 				break;
 			case 2:
-				$data['status'] = __('url is visited', 'wp_gdpr');
+				$data['status'] = __( 'url is visited', 'wp_gdpr' );
 				break;
 		}
 
@@ -156,7 +153,7 @@ class Controller_Menu_Page {
 			foreach ( $_REQUEST['gdpr_emails'] as $single_address ) {
 				$single_address = sanitize_email( $single_address );
 				$to             = $single_address;
-				$subject        = __('Data request', 'wp_gdpr');
+				$subject        = __( 'Data request', 'wp_gdpr' );
 				//TODO prevent duplicates
 				$request = $this->get_request_gdpr_by_email( $single_address );
 
@@ -223,7 +220,7 @@ class Controller_Menu_Page {
 		 * set notice
 		 */
 		$notice = Gdpr_Container::make( 'wp_gdpr\lib\Appsaloon_Notice' );
-		$notice->set_message( __('E-mail send', 'wp_gdpr') );
+		$notice->set_message( __( 'E-mail send', 'wp_gdpr' ) );
 		$notice->register_notice();
 	}
 
@@ -241,17 +238,58 @@ class Controller_Menu_Page {
 	public function build_table_with_delete_requests() {
 
 		global $wpdb;
+		$table_name = $wpdb->prefix . Appsaloon_Customtables::DELETE_REQUESTS_TABLE_NAME;
 
-		$query = "SELECT * FROM {$wpdb->prefix}gdpr_del_requests";
+		$query = "SELECT * FROM $table_name";
 
 		$requests = $wpdb->get_results( $query, ARRAY_A );
+		$requests = array_map( array( $this, 'add_delete_checkbox' ), $requests );
+		$requests = array_map( array( $this, 'reduce_comments_to_string' ), $requests );
 
 		$table = new Appsaloon_Table_Builder(
-			array( __('id', 'wp_gdrp'), __('e-mail', 'wp_gdrp'), __('comments(ID)', 'wp_gdrp'), __('requested at', 'wp_gdrp')),
+			array(
+				__( 'id', 'wp_gdrp' ),
+				__( 'e-mail', 'wp_gdrp' ),
+				__( 'comments(ID)', 'wp_gdrp' ),
+				__( 'requested at', 'wp_gdrp' ),
+				__( 'delete', 'wp_gdrp' )
+			),
 			$requests
-			, array($this->get_delete_form_content()) );
+			, array( $this->get_delete_form_content() ) );
 
 		$table->print_table();
+
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function get_delete_form_content() {
+		ob_start();
+		$controller = $this;
+		include_once GDPR_DIR . 'view/admin/delete-comments-form.php';
+
+		return ob_get_clean();
+	}
+
+	public function add_delete_checkbox( $request ) {
+		$request['checkbox'] = $this->create_checkbox_for_single_delete_row( $request['ID'] );
+
+		return $request;
+	}
+
+	public function create_checkbox_for_single_delete_row( $id ) {
+		return '<input type="checkbox" form="gdpr_form"  name="gdpr_emails[]" value="' . $id . '">';
+	}
+
+	public function reduce_comments_to_string( $item ) {
+		$item['comments'] = array_reduce( unserialize( $item['comments'] ), function ( $carry, $item ) {
+			return $carry . $item . ",";
+		} );
+		$item['comments'] = substr( $item['comments'], 0, - 1 );
+
+		return $item;
 
 	}
 
@@ -269,7 +307,7 @@ class Controller_Menu_Page {
 		$plugins = $this->filter_plugins( $plugins );
 
 		$table = new Appsaloon_Table_Builder(
-			array( __('plugin name', 'wp_gdpr') ),
+			array( __( 'plugin name', 'wp_gdpr' ) ),
 			$plugins
 			, array() );
 
